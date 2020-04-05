3e7554a7a86f6413cbb12a7501cf07c937672305
sstrac <s.r.strachan@outlook.com>
2020-04-05 20:33:03 +0100

    first image switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+const notes = ['C4','D4','E4','F4','G4','A4','B4','C5','D5','E5','F5','G5']
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NotePractice';
+    note='G5'
+    visible = false;
+
+    getRandomNote = () => {
+      return notes[Math.floor(Math.random() * Math.floor(notes.length))]
+    }
+
+    nextNote = () => {
+      this.note = this.getRandomNote()
+    }
 }
